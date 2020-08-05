95f780d274471f4c90ace585bb425b4dddd4962c
Stephen Junge <steve.junge@gmail.com>
2020-08-05 20:55:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 def reverse_each_word_with_each(string)
   new_array = []
-  new_array = string.split(", ")
+  new_array = string.split
   new_array.each do |word|
     return word.reverse
   end
-new_array.join(",")
+new_array.join(" ")
 new_array
 end  
   
