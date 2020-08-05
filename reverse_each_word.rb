6fe03ab643d6189e6eee028dac82c9db501db6f9
Stephen Junge <steve.junge@gmail.com>
2020-08-05 20:58:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,5 +8,13 @@ new_array.join(" ")
 new_array
 end  
   
+def reverse_each_word_with_each(string)
+  new_array = []
+  new_array = string.split
+  new_array.collect do |word|
+    return word.reverse
+  end
+new_array.join(" ")
+end  
   
   
